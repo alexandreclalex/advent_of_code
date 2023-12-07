b5d7cf88963e5d92e6f0e94a33a5b2fa79fb5e80
Alexandre Clavel <alexandre.clavel.2000@gmail.com>
2023-12-06 19:24:28 -0600

    Adding code for 2022 problem 4

---
+++ 2022/4/part2.py
@@ -0,0 +1,15 @@
+
+
+with open('input.txt', 'r') as f:
+    raw_pairs = [line[:-1].split(',') for line in f]
+
+pairs = []
+for pair in raw_pairs:
+    pairs.append([list(map(int, x.split('-'))) for x in pair])
+
+counter = 0
+for r1, r2 in pairs:
+    if r1[0] <= r2[0] <= r1[1] or r2[0] <= r1[0] <= r2[1]:
+        counter += 1
+
+print(counter)
