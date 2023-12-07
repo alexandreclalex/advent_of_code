761920cffa5bbf7bb582fae8b6de68db30d7c022
Alexandre Clavel <alexandre.clavel.2000@gmail.com>
2023-12-06 19:24:28 -0600

    Adding code for 2021 problem 6

---
+++ 2021/6/part1.py
@@ -0,0 +1,23 @@
+DESIRED_DAYS = 80
+
+with open('input.txt', 'r') as f:
+    arr = list(map(int, f.readline().split(',')))
+    
+counts = [0]*9
+for value in arr:
+    counts[value] += 1
+
+def do_day(fish_counts):
+    zero_count = fish_counts[0]
+    new_counts = fish_counts[1:] + [0]
+    new_counts[8] += zero_count
+    new_counts[6] += zero_count
+    return new_counts
+
+
+days = [counts]
+
+while len(days) <= DESIRED_DAYS:
+    days.append(do_day(days[-1]))
+
+print(sum(days[-1]))
